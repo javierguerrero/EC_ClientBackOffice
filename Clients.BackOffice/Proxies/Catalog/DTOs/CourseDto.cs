f2cf2f391f331694f36df660da62a8bcfb6eb81f
jguerrero <jguerrero@goiar.com>
2020-05-11 22:09:28 -0500

    maquetado cabecera

---
+++ Clients.BackOffice/Proxies/Catalog/DTOs/CourseDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clients.BackOffice.Proxies.Catalog.DTOs
+{
+    public class CourseDto
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+    }
+}
